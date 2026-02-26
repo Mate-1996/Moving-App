d4db60dd33baa8e337778108797db962dddc2ea5
Hemifer <pistillityler@icloud.com>
2026-02-25 21:05:09 -0500

    Improved UI and added address continuity

---
+++ Moving-App/Views/MoveRequestConfirmationView.swift
@@ -0,0 +1,18 @@
+//
+//  MoveRequestConfirmationView.swift
+//  Moving-App
+//
+//  Created by user285578 on 2/25/26.
+//
+
+import SwiftUI
+
+struct MoveRequestConfirmationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoveRequestConfirmationView()
+}
